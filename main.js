06760b2240cdc08d9d60b1a2a91c4859f501d9c6
Hisham-Shakeel <162502024+Hisham-Shakeel@users.noreply.github.com>
2024-03-14 10:37:49 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+// Add an Event Listener to the button to run the placeOrder function
+document.getElementById("btn1").addEventListener("click", btn1Clicked);
+document.getElementById("btn2").addEventListener("click", btn2Clicked);
+document.getElementById("btn3").addEventListener("click", btn3Clicked);
+
+// Store order code in a function
+function btn1Clicked() {
+    alert("...together.");
+}
+
+function btn2Clicked() {
+    alert("...short.");
+}
+
+function btn3Clicked() {
+    alert("...both worls.");
+}
